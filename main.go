607b60078e5d11e48e14151142dbc750c4e1bd91
tOspechceva <t.ospechceva@mail.ru>
2026-02-17 13:14:49 +0300

    finish models

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"task-service/config"
+	"task-service/database"
 	"task-service/handlers"
 
 	"github.com/gin-gonic/gin"
@@ -24,17 +25,32 @@ func main() {
 
 	defer db.Close()
 
+	// =========================
+	// МИГРАЦИИ
+	// =========================
+	err = database.RunMigrations(db)
+	if err != nil {
+		log.Fatal("Ошибка миграции:", err)
+	}
+
+	// =========================
+	// SEED DATA
+	// =========================
+	err = database.SeedData(db)
+	if err != nil {
+		log.Fatal("Ошибка seed:", err)
+	}
+
+	log.Println("База данных готова")
+
 	router := gin.Default()
 
 	router.GET("/health", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"status": "ok",
-		})
+		c.JSON(200, gin.H{"status": "ok"})
 	})
 
 	router.GET("/tasks", handlers.GetTasks)
 
 	log.Println("Server started on :3003")
-
 	router.Run(":3003")
 }
